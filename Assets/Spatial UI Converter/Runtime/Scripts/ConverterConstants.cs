40988c5ce6576efef30f4bcea17e584ca5c93501
YanxiLiu <951159548@qq.com>
2022-04-20 20:30:44 +0200

    changed package structure, added path utilities

--- Assets/Spatial UI Converter/Runtime/Scripts/ConverterConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace SpatialUIConverter {
-    internal static class ConverterConstants {
-        public const string MRTKVersion = "2.7.3";
-    }
-}
